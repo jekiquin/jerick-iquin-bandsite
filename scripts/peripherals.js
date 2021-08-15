95f99bbb0bc271a912e7abfaffbe043f8debcfdb
JekIquin <jci.dev.0@gmail.com>
2021-08-15 14:36:23 -0700

    added documentation on peripherals

--- scripts/peripherals.js
+++ scripts/peripherals.js
@@ -1,3 +1,16 @@
+/*
+createElement:
+Description: Creates an element, adds attributes to the element and then appends created element to the parent container
+Parameters:
+    - element: element tag that needs to be created
+    - parent: container where the element tag is to be appended
+    - elementAttributes: element attributes (eg. classList, innerText, src, etc)
+    - append:
+        true: append element
+        false: prepend element
+Return Value:
+    - created element object
+*/
 export function createElement(element, parent='document', elementAttributes={}, append = true) {
     const elementObj = document.createElement(element);
     for (let attribute in elementAttributes) {
@@ -25,6 +38,14 @@ export function createElement(element, parent='document', elementAttributes={}, 
     return elementObj;
 };
 
+/*
+createElement:
+Description: Applies proper case to a word
+Parameters:
+    - string: word to apply proper case to
+Return Value:
+    - Proper case formatted version of the input word.
+*/
 export function capitalize(string) {
     return string[0].toUpperCase() + string.slice(1);
 }
